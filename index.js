5d98dcaf08a6e8be49e73f83b65efd1d71a15830
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-04-08 15:43:42 +0200

    js theme customization

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 //sidebar
-const menuItem = document.querySelectorAll('.menu-item');
+const menuItems = document.querySelectorAll('.menu-item');
 
 // Messages
 const messagesNotification = document.querySelector('#messages-notification');
 const messages = document.querySelector('.messages');
-const message = messages.querySelectorAll('.messages');
-const messageSearch = document.querySelector('.#message-search');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
 
+
+// theme
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
 
 // ============= sidebar =================
 
 // remove active class from all menu items
 const changeActiveItem = () => {
-    menuItem.forEach(item => {
+    menuItems.forEach(item => {
         item.classList.remove('active');
     })
 }
-menuItem.forEach(item => {
+menuItems.forEach(item => {
     item.addEventListener('click', () => {
         changeActiveItem();
         item.classList.add('active');
@@ -33,7 +37,16 @@ menuItem.forEach(item => {
 
 //searches chat messages
 const searchMessage = () => {
-    
+    const val = messageSearch.value.toLowerCase();
+    console.log(val);
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1) {
+            chat.style.display = 'flex';
+        } else {
+            chat.style.display = 'none';
+        }
+    })
 }
 
 //search chat
@@ -42,8 +55,29 @@ messageSearch.addEventListener('keyup', searchMessage);
 // highlight message when clicked
 messagesNotification.addEventListener('click', () => {
     messages.style.boxShadow = '0 0 1rem var(--color-primary)';
-    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    messagesNotification.querySelectorAll('.notification-count').style.display = 'none';
     setTimeout(() => {
         messages.style.boxShadow = 'none';
     }, 2000);
 })
+
+
+// theme customization
+
+//opens modal
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';
+}
+
+//closes modal
+const closeThemeModal = (e) => {
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display = 'none';
+    }
+
+}
+
+//close modal
+themeModal.addEventListener('click', closeThemeModal);
+
+theme.addEventListener('click', openThemeModal);
