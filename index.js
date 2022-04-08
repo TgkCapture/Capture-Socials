5f7b059513f8ebb2e9c081e82561519a4e96acdc
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-04-08 12:12:17 +0200

    message functionality

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const menuItem = document.querySelectorAll('.menu-item');
 // Messages
 const messagesNotification = document.querySelector('#messages-notification');
 const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.messages');
+const messageSearch = document.querySelector('.#message-search');
+
 
 // ============= sidebar =================
 
@@ -27,6 +30,20 @@ menuItem.forEach(item => {
 })
 
 // =============== Messages =================
+
+//searches chat messages
+const searchMessage = () => {
+    
+}
+
+//search chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+// highlight message when clicked
 messagesNotification.addEventListener('click', () => {
     messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
 })
